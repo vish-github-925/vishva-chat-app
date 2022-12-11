69a4e1f7ff0b188b3625b379764d90cc6547a8c9
= <uppunuthula925@gmail.com>
2022-12-11 22:58:28 +0530

    Chat app finished v1

--- backend/config/dbConn.js
+++ backend/config/dbConn.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI_DEV);
+    const conn = await mongoose.connect(process.env.MONGO_URI_PROD);
     console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
     console.log(`Error connecting to MongoDB ${error.message}`.red.underline);
